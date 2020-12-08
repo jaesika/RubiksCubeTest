4e66de12d46e2f0ecf32fd5c93633e378239ef16
yjs <wotlr789@naver.com>
2020-12-08 22:00:57 +0900

    단어 밀어내기 기능 추가
    단어 밀어내기 기능을 위해서 pushWord() 메소드를 추가 하였다

--- src/step1/WordPush.java
+++ src/step1/WordPush.java
@@ -15,7 +15,10 @@ public class WordPush {
             System.out.print("단어를 하나 입력 > ");
             word = scan.nextLine();
             if(word.contains(" ")) {
-                System.out.println("단어를 하나만 입력해주세요.\n");
+                System.out.println("단어를 하나만 입력해 주세요.\n");
+                continue;
+            } else if(word.length()==0) {
+                System.out.println("단어 입력이 안되었습니다.\n");
                 continue;
             }
             System.out.print("-100<=N<100의 정수 하나를 입력 > ");
@@ -23,11 +26,11 @@ public class WordPush {
             try {
                 countInt = Integer.parseInt(count);
             } catch (NumberFormatException e) {
-                System.out.println("숫자 입력이 잘못되었습니다.\n");
+                System.out.println("숫자 입력이 잘못 되었습니다.\n");
                 continue;
             }
-            if(countInt<=-100 || 100<countInt) {
-                System.out.println("-100이상 100미만의 숫자를 입력해주세요\n");
+            if(countInt<-100 || 100<=countInt) {
+                System.out.println("-100이상 100미만의 숫자를 입력해 주세요\n");
                 continue;
             }
 
@@ -41,5 +44,26 @@ public class WordPush {
             }
         }
         scan.close();
+
+        String result = pushWord(word, countInt, direction);
+        System.out.println("result = " + result);
+    }
+
+    static String pushWord(String word, int count, String direction) {
+        StringBuilder result = new StringBuilder();
+        int length = word.length();
+        int absValue = Math.abs(count);
+        int index = absValue%length;
+
+        // 오른쪽으로 밀어내는 경우
+        if((direction.equalsIgnoreCase("R") && count>=0) || (direction.equalsIgnoreCase("L") && count<0)) {
+            result.append(word, (length-index), length);
+            result.append(word, 0, (length-index));
+        } else { // 왼쪽으로 밀어내는 경우
+            result.append(word, index, length);
+            result.append(word, 0, index);
+        }
+
+        return result.toString();
     }
 }
