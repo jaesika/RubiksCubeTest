49b8857d789ca8f5b9ffcfcecbb4a33f781e8639
yjs <wotlr789@naver.com>
2020-12-10 14:39:10 +0900

    사용자 입력 기능 추가
    사용자의 입력을 받기 위해 입력을 받고 입력에 대한 검증 까지 처리하는 기능을 추가 하였다

---
+++ src/step3/RubiksCubeMain.java
@@ -0,0 +1,43 @@
+package step3;
+
+import java.util.Scanner;
+
+public class RubiksCubeMain {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.print("CUBE> ");
+            input = scan.nextLine();
+            System.out.println();
+            if (!validInput(input)) {
+                continue;
+            } else if (isClose(input)) {
+                break;
+            }
+        }
+        scan.close();
+    }
+
+    private static boolean isClose(String input) {
+        if (input.equals("Q")) {
+            System.out.println("GoodBye~~");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean validInput(String actionList) {
+        try {
+            if(actionList.contains(" ") ||  actionList.length()==0) {
+                throw new IllegalStateException();
+            }
+        } catch (IllegalStateException e) {
+            System.out.println("동작을 다시 입력해주세요. ");
+            return false;
+        }
+        return true;
+    }
+}
